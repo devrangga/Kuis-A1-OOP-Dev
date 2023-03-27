bf7bd29af4356d7e6d8df9be7e1a0c2e6fabf916
devrangga <128337038+devrangga@users.noreply.github.com>
2023-03-27 15:56:51 +0700

    Soal ke 2

---
+++ Person.java
@@ -0,0 +1,29 @@
+package QuizOOP2;
+public class Person {
+    private String name, address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
